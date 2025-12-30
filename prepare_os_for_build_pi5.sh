6755b486bfe181df759923faafb6cc625dc089e4
waverage <buorn2011@gmail.com>
2025-12-30 21:03:30 +0100

    added fixip service

--- prepare_os_for_build_pi5.sh
+++ prepare_os_for_build_pi5.sh
@@ -15,10 +15,22 @@ sudo apt install libsdl2-dev -y
 
 sudo apt install libcairo2-dev pkg-config -y
 
+cd /home/pi
+
+git clone https://github.com/waverage/RubyFPV.git
+
+mv RubyFPV ruby
+
+sudo chmod 777 ruby
+
+cd ruby
+
+git checkout pi5
+
 sudo chmod 777 /home/pi/ruby/start.sh
 
-sudo cp /home/pi/ruby/systemd_ruby.service /etc/systemd/system/ruby.service
-sudo systemctl enable ruby.service
+sudo cp /home/pi/ruby/systemd_fixip.service /etc/systemd/system/fixip.service
+sudo systemctl enable fixip.service
 
 # change /boot/firmware/config.txt
 # put binaries to /home/pi/ruby/
