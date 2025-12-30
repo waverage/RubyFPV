ebe2950346a65aae491dd7129b213fee0575ee23
waverage <buorn2011@gmail.com>
2025-12-30 12:46:50 +0100

    fix start.sh

--- start.sh
+++ start.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
 
 touch /tmp/debug
+
 echo "Mounting file systems..."
+
 #mount -o remount,rw /
-#mount -o remount,rw /boot/firmware
+#mount -o remount,rw /boot
 echo "adding eth0 ip for debugging..."
+
 ip link set eth0 up
+
 ip addr add 192.168.1.2/24 dev eth0 2>/dev/null || true
+
 ip addr show eth0
+
 echo "stop network service..."
+
 #systemctl stop NetworkManager
 #systemctl stop wpa_supplicant
-rfkill unblock wifi
+#rfkill unblock wifi
+echo "Fixing semaphores..."
+# Clean up POSIX semaphores
+rm -f /dev/shm/sem.RUBY*
+# Clean up System V semaphores (just in case)
+for semid in $(ipcs -s | awk '/0x/ {print $2}'); do ipcrm -s $semid; done
 
 echo "Launching..."
+
 cd /home/pi/ruby
+
 ./ruby_start
+
 echo "Launch done."
 
 sleep 2
+
 mkdir -p logs
 touch logs/log_system.txt
 tail -f logs/log_system.txt
